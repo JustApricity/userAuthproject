3a2a6d594815a7d577a62016f5431f5ad6fe60f6
Apricity <101598622+JustApricity@users.noreply.github.com>
2023-03-10 11:17:21 -0800

    commit 2

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,6 @@ router.get('/item/incomplete/:id',  todoController.markItemIncomplete);
 
 router.get('/register', userController.renderRegistration);
 router.post('/register', userController.register);
+router.get('/login', userController.renderLogin);
+router.post('/login', userController.authenticate);
 module.exports = router;
